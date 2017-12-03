968837510b862d829a81f61b869d9faf03cc8e22
habeeb <habibmail31@yahoo.com>
2017-12-03 08:41:05 +0100

    new update

--- mongoose.js
+++ mongoose.js
@@ -1,4 +1,4 @@
 var config = require("./config/config");
 
-var mongoose = require("mongoose").connect(config.dbURL, { useMongoClient: true });
+var mongoose = require("mongoose").connect(config.dbURL, { useMongoClient: true }).then((success)=>console.log(success),(failed)=>console.log(failed)).catch((failed)=>console.log(`error elsewhere: ${failed}`))
 module.exports = mongoose;
