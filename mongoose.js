29b99475106c8ecf94f65c19e5ef3fdb7367f083
habeeb <habibmail31@yahoo.com>
2017-12-03 10:56:53 +0100

    new update

--- mongoose.js
+++ mongoose.js
@@ -2,5 +2,5 @@ var config = require("./config/config");
 
 
 var mongoose = require("mongoose");
-mongoose.connect(config.dbURL, { useMongoClient: true }).then((success)=>console.log(success),(failed)=>console.log(failed)).catch((failed)=>console.log(`error elsewhere: ${failed}`))
+mongoose.connect("mongodb://guest:guest@ds159845.mlab.com:59845/twinkasrtc", { useMongoClient: true }).then((success)=>console.log(success),(failed)=>console.log(failed)).catch((failed)=>console.log(`error elsewhere: ${failed}`))
 module.exports = mongoose;
